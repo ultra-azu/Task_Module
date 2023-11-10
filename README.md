306fbc42c082390927b6f4dd5e7043f258110a87
Cruiz102 <cruiznavarro44@gmail.com>
2023-11-09 22:54:02 -0400

    add dockerfile and dependencies

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 TODOs:
 1. Implement all the dependencies  and  ros Topics dependencies  in the Docker.
-2. Fix the docker bug that do not let run the Pandas Tutorial Moveit Package in Non WSL(Windows Subsystem Linux) OS.
 2. Finish the launch files implementations.
 3. Create a Ros Package for the Submarine.
 4. Finish the State Machine Containers for each task.
--- tasks/movement.py
+++ tasks/movement.py
@@ -2,18 +2,9 @@
 
 import rospy
 import smach
-import smach_ros
-from moveit_commander import RobotCommander, PlanningSceneInterface
-from moveit_commander import MoveGroupCommander
+from uuv_control_msgs.srv import GoTo
 from geometry_msgs.msg import PoseStamped
-
-
-import rospy
-from sensor_msgs.msg import Image, Imu
-from geometry_msgs.msg import TwistStamped
-from cv_bridge import CvBridge, CvBridgeError
-from tf.transformations import quaternion_from_euler
-
+from uuv_control_msgs.msg import Waypoint
 
 class UpdatePoseState(smach.State):
     def __init__(self, group_name, target_pose, debug=False):
@@ -23,8 +14,8 @@ class UpdatePoseState(smach.State):
         self.camera_data = None
         self.imu_data = None
         self.dvl_data = None
-        self.cv_bridge = CvBridge()
         self.target_pose = target_pose
+        self.goto_service = rospy.ServiceProxy('/your_goto_service', GoTo)
         self.debug = False
 
         # Subscribe to the camera, IMU, and DVL topics
@@ -45,27 +36,30 @@ class UpdatePoseState(smach.State):
         self.dvl_data = data
 
     def execute(self, userdata):
-        move_group = MoveGroupCommander(self.group_name)
+        # Prepare the waypoint message
+        waypoint = Waypoint()
+        waypoint.header.stamp = rospy.Time.now()
+        waypoint.header.frame_id = "world"  # Change this as per your coordinate frame
+        waypoint.point = self.target_pose.position
+        waypoint.max_forward_speed = 1.0
+        waypoint.heading_offset = 0.0
+        waypoint.use_fixed_heading = False
+        waypoint.radius_of_acceptance = 2.0  # Set as per requirement
 
-        # Set the target pose for the end effector
-        self.target_pose.header.frame_id = move_group.get_planning_frame()
+        # Prepare the GoTo service request
+        goto_request = GoToRequest()
+        goto_request.waypoint = waypoint
+        goto_request.interpolator = 'linear'
 
-        if self.debug:
-            rospy.loginfo("Target pose: {0}".format(self.target_pose))
-            return "success"
-
-        # Plan and execute the movement
-        move_group.set_pose_target(self.target_pose)
-        plan = move_group.go(wait=True)
-        
-        if self.camera_data is None and self.imu_data is None and self.dvl_data is None:
-            # Process sensor data
-            return 'preempted'
-        if plan:
-            return 'success'
-        else:
+        try:
+            response = self.goto_service(goto_request)
+            if response.success:
+                return 'success'
+            else:
+                return 'aborted'
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: %s" % e)
             return 'aborted'
-
 
 class UpdatePoseToObjectState(UpdatePoseState):
     def __init__(self, group_name, object_topic, desired_object_name,debug):
@@ -83,65 +77,59 @@ class UpdatePoseToObjectState(UpdatePoseState):
                 self.object_data = data.pose
 
     def execute(self, userdata):
-        move_group = MoveGroupCommander(self.group_name)
-
         if self.object_data is not None:
-            # Set the target pose for the end effector to the object pose
-            self.target_pose = self.object_data
+            # Prepare the waypoint message for the object's position
+            waypoint = Waypoint()
+            waypoint.header.stamp = rospy.Time.now()
+            waypoint.header.frame_id = "world"
+            waypoint.point = self.object_data.position  # Assuming object_data is a Pose
+            # ... set other waypoint attributes ...
 
-            # If debug is enabled, print the object pose
-            if self.debug:
-                print(f"Moving to: {self.desired_object_name}: {self.object_data}")
-                return 'success'
-            
+            # Prepare and call GoTo service
+            goto_request = GoToRequest()
+            goto_request.waypoint = waypoint
+            # ... set other request attributes ...
 
-            # Plan and execute the movement
-            move_group.set_pose_target(self.target_pose)
-            plan = move_group.go(wait=True)
-            
-            if plan:
-                return 'success'
-            else:
+            try:
+                response = self.goto_service(goto_request)
+                return 'success' if response.success else 'aborted'
+            except rospy.ServiceException as e:
+                rospy.logerr("Service call failed: %s" % e)
                 return 'aborted'
         else:
-            if self.camera_data is None and self.imu_data is None and self.dvl_data is None:
-                # Process sensor data
-                return 'preempted'
-            else:
-                return 'aborted'
+            return 'aborted'
 
 
 class Rotate90DegreesState(UpdatePoseState):
     def __init__(self, group_name, debug=False):
         super(Rotate90DegreesState, self).__init__(group_name, debug)
+        pass
 
     def execute(self, userdata):
-        move_group = MoveGroupCommander(self.group_name)
-
         # Get the current pose
-        current_pose = move_group.get_current_pose().pose
+        current_pose = # obtain current pose
 
-        # Compute the new orientation after a 90-degree rotation around the Z-axis
-        new_orientation = quaternion_from_euler(0, 0, current_pose.orientation.z + 1.5708)  # 1.5708 radians = 90 degrees
+        # Compute new orientation (example: rotate 90 degrees around Z-axis)
+        new_orientation = # calculate new orientation
 
-        # Set the target pose for the end effector
-        target_pose = PoseStamped()
-        target_pose.header.frame_id = move_group.get_planning_frame()
-        target_pose.pose.position = current_pose.position
-        target_pose.pose.orientation = Quaternion(*new_orientation)
+        # Prepare the waypoint for the new orientation
+        waypoint = Waypoint()
+        waypoint.header.stamp = rospy.Time.now()
+        waypoint.header.frame_id = "world"
+        waypoint.point = current_pose.position
+        waypoint.heading_offset = # heading offset for 90-degree rotation
+        # ... set other waypoint attributes ...
 
-        if self.debug:
-            print(f"Rotating 90 Degrees: {target_pose}")
-            return 'success'
+        # Prepare and call GoTo service
+        goto_request = GoToRequest()
+        goto_request.waypoint = waypoint
+        # ... set other request attributes ...
 
-
-        # Plan and execute the movement
-        move_group.set_pose_target(target_pose)
-        plan = move_group.go(wait=True)
-        
-        if plan:
-            return 'success'
-        else:
+        try:
+            response = self.goto_service(goto_request)
+            return 'success' if response.success else 'aborted'
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: %s" % e)
             return 'aborted'
         
 # TODO: FIx the Lower Depth Class
