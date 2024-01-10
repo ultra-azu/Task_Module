bb7a9898bc61106a74a5af97b237e68600ec2050
Cruiz102 <cruiznavarro44@gmail.com>
2024-01-10 16:54:11 -0400

    added more data to the sharedData struct and finish test_camera_input test.

--- tasks/src/data.py
+++ tasks/src/data.py
@@ -1,11 +1,12 @@
-
 import yaml
 import rospy
+from sensor_msgs.msg import Imu, Image, CameraInfo
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float32
-# from uuv_sensor_ros_plugins_msgs.msg import DVL_Message
+from zed_interfaces.msg import ObjectsStamped, RGBDSensors
 from nav_sensors.msg import DVL_MSG
-from zed_interfaces.msg import ObjectsStamped
-from zed_interfaces.msg import RGBDSensors
+
 
 
 def read_yaml_file(file_path):
@@ -13,16 +14,15 @@ def read_yaml_file(file_path):
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
-            print(exc)
+            rospy.logerr(exc)
             return None
-
-
 
 # Class for accesing the Robots Data in the state machine
 class SharedData:
     def __init__(self):
-        #TODO: Add the 
-        self.zed_data = {"ObjectsStamped": None, "RGBDSensors": None, "Pose": None}
+        self.zed_data = {"objects_stamped": None, "camera_info": None, "imu": None, 
+                         "image": None, "pose": None, "odom": None, "path_odom": None, 
+                         "path_map": None}
         self.dvl_data = None
         self.imu_data = {'yaw': None, 'pitch': None, 'roll': None}
         # Add more attributes as needed
@@ -35,12 +35,35 @@ class SharedData:
 # Global variable for accesing the shared_data 
 shared_data = SharedData()
 
+###########################################
+    # Zed Camera Callbacks
 def zed_objects_callback(msg):
-    shared_data.zed_data['ObjectsStamped'] = msg
+    shared_data.zed_data['objects_stamped'] = msg
 
-def zed_rgbd_callback(msg):
-    shared_data.zed_data["RGBDSensors"] = msg
+def zed_camera_info_callback(msg):
+    shared_data.zed_data['camera_info'] = msg
 
+def zed_imu_callback(msg):
+    shared_data.zed_data['imu'] = msg
+
+def zed_image_callback(msg):
+    shared_data.zed_data['image'] = msg
+
+def zed_pose_callback(msg):
+    shared_data.zed_data['pose'] = msg
+
+def zed_odom_callback(msg):
+    shared_data.zed_data['odom'] = msg
+
+def zed_path_odom_callback(msg):
+    shared_data.zed_data['path_odom'] = msg
+
+def zed_path_map_callback(msg):
+    shared_data.zed_data['path_map'] = msg
+
+###########################################
+    
+#DVL and IMU from the Hydrus repository
 def dvl_callback(msg):
     shared_data.dvl_data = msg
 
@@ -53,22 +76,41 @@ def imu_pitch_callback(msg):
 def imu_roll_callback(msg):
     shared_data.imu_data['roll'] = msg.data
 
+
+
+
+#############################
+    # Calculated Pose
+#Either Simulation or  real IMU calculation
 def pose_callback(msg):
     shared_data.pose = msg
 
+###############################
 
 
+
+
+# Initialize subscribers
 def initialize_subscribers(topics_file):
     topics_info = read_yaml_file(topics_file)
     if topics_info is None:
-        print("Failed to read YAML file or file is empty.")
+        rospy.logerr("Failed to read YAML file or file is empty.")
     else:
-        print("YAML file read successfully:", topics_info)
-        print(topics_info['zed_camera']['ObjectsStamped'])
+        rospy.loginfo("YAML file read successfully.")
 
-    rospy.Subscriber(topics_info['zed_camera']['ObjectsStamped'],ObjectsStamped, zed_objects_callback)  # Update the message type
-    rospy.Subscriber(topics_info['zed_camera']['RGBDSensors'],RGBDSensors,  zed_rgbd_callback)  # Update the message type
-    rospy.Subscriber(topics_info['dvl']['DVL_Message'], DVL_MSG, dvl_callback)  # Update the message type
-    rospy.Subscriber(topics_info['imu']['roll'],Float32, imu_roll_callback)
-    rospy.Subscriber(topics_info['imu']['yaw'],Float32,  imu_yaw_callback)
-    rospy.Subscriber(topics_info['imu']['pitch'],Float32 , imu_pitch_callback)
+        # Zed Camera Subscribers
+        rospy.Subscriber(topics_info['zed_camera']['objects_stamped'], ObjectsStamped, zed_objects_callback)
+        rospy.Subscriber(topics_info['zed_camera']['camera_info'], CameraInfo, zed_camera_info_callback)
+        rospy.Subscriber(topics_info['zed_camera']['imu'], Imu, zed_imu_callback)
+        rospy.Subscriber(topics_info['zed_camera']['image'], Image, zed_image_callback)
+        rospy.Subscriber(topics_info['zed_camera']['pose'], PoseStamped, zed_pose_callback)
+        rospy.Subscriber(topics_info['zed_camera']['odom'], Odometry, zed_odom_callback)
+        rospy.Subscriber(topics_info['zed_camera']['path_odom'], Path, zed_path_odom_callback)
+        rospy.Subscriber(topics_info['zed_camera']['path_map'], Path, zed_path_map_callback)
+
+        rospy.Subscriber(topics_info['dvl']['DVL_Message'], DVL_MSG, dvl_callback)  # Update the message type
+        rospy.Subscriber(topics_info['imu']['roll'],Float32, imu_roll_callback)
+        rospy.Subscriber(topics_info['imu']['yaw'],Float32,  imu_yaw_callback)
+        rospy.Subscriber(topics_info['imu']['pitch'],Float32 , imu_pitch_callback)
+
+
--- tasks/test/test_camera.py
+++ tasks/test/test_camera.py
@@ -6,11 +6,11 @@ import rospy
 import os
 import rosbag
 import yaml
-import unittest
 import rospy
 from tasks.src.data import shared_data, initialize_subscribers
 class Camera_Behaviours(unittest.TestCase):
     def setUp(self):
+        print("Setting Up Camera Test")
 
         rospy.init_node('test_camera_node', anonymous=True)
         file_path = os.path.join(os.path.dirname(__file__), 'test_data/test_topics.yml')
@@ -19,10 +19,17 @@ class Camera_Behaviours(unittest.TestCase):
         # outcome = sm.execute()
 
     def test_camera_input(self):
+        # wait 5 seconds for data to be published
+        rospy.sleep(5)
         print("Testing Camera Input")
-        self.assertIsNotNone(shared_data.zed_data['ObjectsStamped'])
-        self.assertIsNotNone(shared_data.zed_data['RGBDSensors'])
-        self.assertIsNotNone(shared_data.dvl_data)
+        self.assertIsNotNone(shared_data.zed_data['objects_stamped'])
+        self.assertIsNotNone(shared_data.zed_data['imu'])
+        self.assertIsNotNone(shared_data.zed_data['image'])
+        self.assertIsNotNone(shared_data.zed_data['camera_info'])
+        self.assertIsNotNone(shared_data.zed_data['pose'])
+        self.assertIsNotNone(shared_data.zed_data['odom'])
+        self.assertIsNotNone(shared_data.zed_data['path_odom'])
+        self.assertIsNotNone(shared_data.zed_data['path_map'])
 
     def tearDown(self):
         # Clean up code here
